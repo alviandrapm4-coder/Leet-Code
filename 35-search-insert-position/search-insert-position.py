30cece61af909f3dc03a8eefe20535ff293a54d7
Alviandra Piero <alviandrapm4@gmail.com>
2026-05-24 11:54:55 +0700

    Time: 0 ms (100.00%) | Memory: 12.9 MB (65.33%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -5,14 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-            if target < nums[0]:
-                return 0
-            if len(nums) == 1:
-                return 1
-            if target > nums[-1]:
-                return len(nums)
-            if nums[i] < target < nums[i+1]:
-                return i+1
+        #create a left and right pointer
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if target == nums[mid]:
+                return mid
+            if target > nums[mid]:
+                l = mid + 1
+            if target < nums[mid]:
+                r = mid - 1
+        return l
