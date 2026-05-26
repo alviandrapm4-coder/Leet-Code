10532fa88029fba36d78ca80238b8499528f999f
Alviandra Piero <alviandrapm4@gmail.com>
2026-05-26 18:31:42 +0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.26%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return int(x ** 0.5)
+        l, r = 0, x
+        res = 0
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if m * m > x:
+                r = m - 1
+            elif m * m < x:
+                l = m + 1
+                res = m
+            else:
+                return m
+        return res
