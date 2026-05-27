c7ea31db4e7fd9b746b490e8f0a509cae41c64e3
Alviandra Piero <alviandrapm4@gmail.com>
2026-05-27 20:51:59 +0700

    Time: 36 ms (31.22%) | Memory: 12.7 MB (77.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,9 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        sLow = s.lower()
-        sFilter = []
-        for char in sLow:
-            if ('a' <= char <= 'z') or ('0' <= char <= '9'):
-                sFilter.append(char)
-        return sFilter == sFilter[::-1]
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+    def alphaNum(self, c):
+        return (ord("a") <= ord(c) <= ord("z") or
+                ord("A") <= ord(c) <= ord("Z") or
+                ord("0") <= ord(c) <= ord("9"))
